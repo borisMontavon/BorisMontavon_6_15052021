cbd6ed858b63d0382be0aaaf0bd50c781a0f8472
boris_montavon <boris.montavon@gmail.com>
2021-05-15 09:38:31 +0200

    Init

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+const mainFolder = './src/scss/*.scss';
+const moduleFolder = './src/scss/*/*/*.scss';
+const destFolder = './src/css/';
+
+gulp.task('styles', () => {
+    return gulp.src(mainFolder)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(destFolder));
+});
+
+gulp.task('clean', () => {
+    return del([
+        './css/*.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
+
+gulp.task('watch', function () {
+    gulp.watch(mainFolder, gulp.series('styles'));
+    gulp.watch(moduleFolder, gulp.series('styles'));
+});
